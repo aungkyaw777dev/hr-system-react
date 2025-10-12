2b99dffd1626568632705c16a1b42f6d1e2810ff
Saw Mon Han <sawmon71293@gmail.com>
2025-10-13 01:02:08 +0630

    minor

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -4,9 +4,9 @@ export default function Header() {
     return (
         <div className="bg-primary flex justify-between items-center text-white h-full">
             <div className='cursor-pointer text-2xl ps-4'>Logo</div>
-            <div className='flex gap-4 text-2xl'>
-                <Bell className='cursor-pointer' />
-                <CircleUser className='ms-4 cursor-pointer' />
+            <div className='flex gap-4 text-2xl me-4'>
+                <Bell className='cursor-pointer me-2' />
+                <CircleUser className='cursor-pointer' />
             </div>
         </div>
     )
