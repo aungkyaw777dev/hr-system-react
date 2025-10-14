be7ac5bd0f4edc1d1047e50ace75c62d80a70b70
Saw Mon Han <sawmon71293@gmail.com>
2025-10-14 15:27:52 +0630

    minor

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const dateFormatter = (date: Date): string => {
+export const dateFormatter = (date: string): string => {
   const dt = new Date(date);
   return isNaN(dt.getTime())
     ? ""
