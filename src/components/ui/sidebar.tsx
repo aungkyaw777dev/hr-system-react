5843b804d18925f1dccbbf8157b703c0d8cc8951
Saw Mon Han <sawmon71293@gmail.com>
2025-10-14 10:43:22 +0630

    responsive for sidebar

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -2,35 +2,39 @@ import { Link } from "react-router-dom";
 import { ChevronDown, ChevronUp } from 'lucide-react';
 import { useState } from "react";
 
-export default function Sidebar() {
-    const [isOpen, setIsOpen] = useState(false);
-    const toggleSubmenu = () => setIsOpen(!isOpen);
-
+interface SidebarProps {
+    isOpen: boolean,
+}
+export default function Sidebar({ isOpen }: SidebarProps) {
+    const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
+    const toggleSubmenu = () => setIsSubMenuOpen(!isSubMenuOpen);
     return (
-        <div className="bg-primary text-white text-2xl h-full flex flex-col items-center gap-2 w-full">
-            <Link to="/dashboard" className="sidebar-btn">Dashboard</Link>
-            <Link to="/role" className="sidebar-btn">Role</Link>
-            <Link to="/menu" className="sidebar-btn"> Menu</Link>
-            <Link to="/role" className="sidebar-btn">Role & Permission</Link>
-            <Link to="/employee" className="sidebar-btn">Employee</Link>
-            <Link to="/backlog" className="sidebar-btn">Backlog Module</Link>
-            <div className="sidebar-btn flex w-full justify-center items-center" onClick={toggleSubmenu}>
-                <span className="me-2 cursor-pointer">Attendance Module</span>
-                <ChevronDown className={`mt-2 text-sm transition-transform duration-300 ${isOpen ? "rotate-180" : "rotate-0"}`} size={14} />
-
+        <div className={`bg-primary text-white text-xl h-full gap-2 w-full transform transition-transform duration-300 ease-in-out
+            lg:translate-x-0 ${isOpen ? "translate-x-0" : "-translate-x-full"}`} >
+            <div className="flex flex-col items-center">
+                <Link to="/dashboard" className="sidebar-btn">Dashboard</Link>
+                <Link to="/role" className="sidebar-btn">Role</Link>
+                <Link to="/menu" className="sidebar-btn"> Menu</Link>
+                <Link to="/role" className="sidebar-btn">Role & Permission</Link>
+                <Link to="/employee" className="sidebar-btn">Employee</Link>
+                <Link to="/backlog" className="sidebar-btn">Backlog Module</Link>
+                <div className="sidebar-btn flex w-full justify-center items-center" onClick={toggleSubmenu}>
+                    <span className="me-2 cursor-pointer">Attendance Module</span>
+                    <ChevronDown className={`mt-2 text-sm transition-transform duration-300 ${isOpen ? "rotate-180" : "rotate-0"}`} size={14} />
+                </div>
+                {isSubMenuOpen &&
+                    <ul className="w-full">
+                        <li className="sidebar-btn">
+                            <Link to="/location" >Location</Link>
+                        </li>
+                        <li className="sidebar-btn">
+                            <Link to="/attendance" >Attendance</Link>
+                        </li>
+                    </ul>
+                }
+                <Link to="/payroll" className="sidebar-btn">Payroll</Link>
+                <Link to="/logout" className="sidebar-btn">Logout</Link>
             </div>
-            {isOpen &&
-                <ul className="w-full">
-                    <li className="sidebar-btn">
-                        <Link to="/location" >Location</Link>
-                    </li>
-                    <li className="sidebar-btn">
-                        <Link to="/attendance" >Attendance</Link>
-                    </li>
-                </ul>
-            }
-            <Link to="/payroll" className="sidebar-btn">Payroll</Link>
-            <Link to="/logout" className="sidebar-btn">Logout</Link>
         </div>
     );
 }
--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -2,20 +2,29 @@ import Header from "../components/ui/header";
 import Sidebar from "../components/ui/sidebar";
 import { Outlet } from "react-router-dom";
 import "../styles/index.css"
+import { useState } from "react";
 export default function MainLayout() {
+
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
     return (
         <div className="flex min-h-screen flex-col">
             <header className="h-[80px]">
-                <Header />
+                <Header onToggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)} />
             </header>
             <div className="flex flex-1">
-                <aside className="w-64">
-                    <Sidebar />
+                <aside className="w-64 lg:block z-20">
+                    <Sidebar isOpen={isSidebarOpen} />
                 </aside>
                 <main className="flex-1">
                     <Outlet />
                 </main>
             </div>
+            {isSidebarOpen && (
+                <div
+                    className="fixed inset-0 bg-black/50 z-10 lg:hidden"
+                    onClick={() => setIsSidebarOpen(false)}
+                />
+            )}
         </div>
     )
 }
