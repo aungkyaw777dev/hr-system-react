22b6c604ecd158b048e547de7f49bc1e9aba59cc
Aster-Shan <thuushanphyoe098@gmail.com>
2025-10-15 21:01:28 +0100

    MenuItem Create and Edit Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import LoginPage from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import MainLayout from "./layout/MainLayout";
+import MainLayout from "./layouts/MainLayout";
 import Attendance from "./pages/Attendance/Index";
 import Location from "./pages/Attendance/Location";
+import Dashboard from "./pages/Dashboard";
+import LoginPage from "./pages/Login";
+import MenuItem from "./pages/Menu/MenuItem";
+import MenuItemCreate from "./pages/Menu/MenuItemCreate";
+import MenuItemEdit from "./pages/Menu/MenuItemEdit";
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes >
+        <Routes>
           <Route path="/" element={<LoginPage />}></Route>
           <Route element={<MainLayout />}>
             <Route path="/dashboard" element={<Dashboard />}></Route>
             <Route path="/attendance" element={<Attendance />}></Route>
             <Route path="/location" element={<Location />}></Route>
+
+            <Route path="/menuitem" element={<MenuItem />}></Route>
+            <Route path="/menuitem/create" element={<MenuItemCreate />} />
+            <Route path="/menuitem/edit" element={<MenuItemEdit />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,36 +1,79 @@
-import { Link } from "react-router-dom";
-import { ChevronDown, ChevronUp } from 'lucide-react';
+import { ChevronDown } from "lucide-react";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Sidebar() {
-    const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
-    const toggleSubmenu = () => setIsSubMenuOpen(!isSubMenuOpen);
-    return (
-        <div>
-            <div className="flex flex-col items-center">
-                <Link to="/dashboard" className="sidebar-btn">Dashboard</Link>
-                <Link to="/role" className="sidebar-btn">Role</Link>
-                <Link to="/menu" className="sidebar-btn"> Menu</Link>
-                <Link to="/role" className="sidebar-btn">Role & Permission</Link>
-                <Link to="/employee" className="sidebar-btn">Employee</Link>
-                <Link to="/backlog" className="sidebar-btn">Backlog Module</Link>
-                <div className="sidebar-btn flex w-full justify-center items-center" onClick={toggleSubmenu}>
-                    <span className="me-2 cursor-pointer">Attendance Module</span>
-                    <ChevronDown className={`mt-2 text-sm transition-transform duration-300 ${isSubMenuOpen ? "rotate-180" : "rotate-0"}`} size={14} />
-                </div>
-                {isSubMenuOpen &&
-                    <ul className="w-full">
-                        <li className="sidebar-btn">
-                            <Link to="/location" >Location</Link>
-                        </li>
-                        <li className="sidebar-btn">
-                            <Link to="/attendance" >Attendance</Link>
-                        </li>
-                    </ul>
-                }
-                <Link to="/payroll" className="sidebar-btn">Payroll</Link>
-                <Link to="/logout" className="sidebar-btn">Logout</Link>
-            </div>
+  const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
+  const toggleSubmenu = () => setIsSubMenuOpen(!isSubMenuOpen);
+
+  const [isSubMenuItemOpen, setIsSubMenuItemOpen] = useState(false);
+  const toglemenuItem = () => setIsSubMenuItemOpen(!isSubMenuItemOpen);
+  return (
+    <div>
+      <div className="flex flex-col items-center">
+        <Link to="/dashboard" className="sidebar-btn">
+          Dashboard
+        </Link>
+        <Link to="/role" className="sidebar-btn">
+          Role
+        </Link>
+        <div
+          className="sidebar-btn flex w-full justify-center items-center"
+          onClick={toglemenuItem}
+        >
+          <span className="me-2 cursor-pointer">Menu</span>
+          <ChevronDown
+            className={`mt-2 text-sm transition-transform duration-300 ${
+              isSubMenuItemOpen ? "rotate-180" : "rotate-0"
+            }`}
+            size={14}
+          />
         </div>
-    );
+        {isSubMenuItemOpen && (
+          <ul className="w-full">
+            <li className="sidebar-btn">
+              <Link to="/menuitem">Menu Item</Link>
+            </li>
+          </ul>
+        )}
+        <Link to="/role" className="sidebar-btn">
+          Role & Permission
+        </Link>
+        <Link to="/employee" className="sidebar-btn">
+          Employee
+        </Link>
+        <Link to="/backlog" className="sidebar-btn">
+          Backlog Module
+        </Link>
+        <div
+          className="sidebar-btn flex w-full justify-center items-center"
+          onClick={toggleSubmenu}
+        >
+          <span className="me-2 cursor-pointer">Attendance Module</span>
+          <ChevronDown
+            className={`mt-2 text-sm transition-transform duration-300 ${
+              isSubMenuOpen ? "rotate-180" : "rotate-0"
+            }`}
+            size={14}
+          />
+        </div>
+        {isSubMenuOpen && (
+          <ul className="w-full">
+            <li className="sidebar-btn">
+              <Link to="/location">Location</Link>
+            </li>
+            <li className="sidebar-btn">
+              <Link to="/attendance">Attendance</Link>
+            </li>
+          </ul>
+        )}
+        <Link to="/payroll" className="sidebar-btn">
+          Payroll
+        </Link>
+        <Link to="/logout" className="sidebar-btn">
+          Logout
+        </Link>
+      </div>
+    </div>
+  );
 }
