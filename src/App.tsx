fa51a0d8faa518d128cee8ef121e913e19c84e29
ZawHtweOo <zawhtweoo888@gmail.com>
2025-10-18 11:17:25 +0630

    progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-import MainLayout from "./layout/MainLayout";
+import MainLayout from "./layouts/MainLayout";
 import Attendance from "./pages/Attendance/Index";
-import Location from "./pages/Attendance/Location";
+import Location from "./pages/Attendance/Location/Index";
+import Employee from "./pages/Employee/Index";
+import EmployeeForm from "./components/employee/EmployeeForm";
+import EmployeeView from "./pages/Employee/View";
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes >
+        <Routes>
           <Route path="/" element={<LoginPage />}></Route>
           <Route element={<MainLayout />}>
             <Route path="/dashboard" element={<Dashboard />}></Route>
             <Route path="/attendance" element={<Attendance />}></Route>
             <Route path="/location" element={<Location />}></Route>
+            <Route path="/employee" element={<Employee />}></Route>
+            <Route path="/employee/new" element={<EmployeeForm />}></Route>
+            <Route path="/employee/edit/:code" element={<EmployeeForm />} />
+            <Route path="/employee/view/:code" element={<EmployeeView />} />
           </Route>
         </Routes>
       </BrowserRouter>
