c956aac46c7cb34f1e13f4d51bd7fa97a92a221a
sawmon71293 <sawmon71293@gmail.com>
2025-10-19 11:37:08 +0630

    create and update

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import BacklogDetail from "./pages/Backlog/Detail";
 import Backlog from "./pages/Backlog/Index";
 import BacklogEdit from "./pages/Backlog/Edit";
 import MainLayout from "./layouts/MainLayout";
-import Attendance from "./pages/Attendance/Index";
+import { AttendanceList } from "./pages/Attendance/Index";
+import { CreateAttendance } from "./pages/Attendance/Create";
+import { UpdateAttendance } from "./pages/Attendance/[id]";
 import Payroll from "./pages/Payroll/Payroll";
 import PayrollCreate from "./pages/Payroll/PayrollCreate";
 import PayrollEdit from "./pages/Payroll/PayrollEdit";
 import PayrollDetail from "./pages/Payroll/PayrollDetail";
-import Location from "./pages/Attendance/Location";
 import Dashboard from "./pages/Dashboard";
 import LoginPage from "./pages/Login";
 import MenuItem from "./pages/Menu/MenuItem";
@@ -22,7 +23,7 @@ import { ProjectEdit } from "./pages/BackLog/Project/ProjectEdit";
 import Employee from "./pages/Employee/Index";
 import EmployeeForm from "./components/employee/EmployeeForm";
 import EmployeeView from "./pages/Employee/View";
-import AttendanceCreate from "./pages/Attendance/Create"
+import Location from "./pages/Attendance/Location/Index";
 function App() {
   return (
     <>
@@ -33,19 +34,30 @@ function App() {
             <Route path="/dashboard" element={<Dashboard />}></Route>
             <Route path="/backlog" element={<Backlog />}></Route>
             <Route path="/backlog/:id" element={<BacklogDetail />} />
-            <Route path="/backlog/create" element={<BacklogCreate/>}></Route>
-            <Route path="/backlog/edit/:id" element={<BacklogEdit/>}></Route>
+            <Route path="/backlog/create" element={<BacklogCreate />}></Route>
+            <Route path="/backlog/edit/:id" element={<BacklogEdit />}></Route>
             <Route path="/project" element={<ProjectList />}></Route>
             <Route path="/projects/new" element={<ProjectCreate />} />
             <Route path="/projects/:id" element={<ProjectDetails />} />
             <Route path="/projects/:id/edit" element={<ProjectEdit />} />
-            <Route path="/attendance" element={<Attendance />}></Route>
-            <Route path="/attendance/create" element={<AttendanceCreate />}></Route>
+            <Route path="/attendance" element={<AttendanceList />}></Route>
+            <Route
+              path="/attendance/create"
+              element={<CreateAttendance />}
+            ></Route>
+            <Route
+              path="/attendance/:code/update"
+              element={<UpdateAttendance />}
+            ></Route>
+
             <Route path="/location" element={<Location />}></Route>
             <Route path="/payroll" element={<Payroll />}></Route>
             <Route path="/payroll/create" element={<PayrollCreate />}></Route>
             <Route path="/payroll/:id/edit" element={<PayrollEdit />}></Route>
-            <Route path="/payroll/:id/detail" element={<PayrollDetail />}></Route>
+            <Route
+              path="/payroll/:id/detail"
+              element={<PayrollDetail />}
+            ></Route>
             <Route path="/menuitem" element={<MenuItem />}></Route>
             <Route path="/menuitem/create" element={<MenuItemCreate />} />
             <Route path="/menuitem/edit" element={<MenuItemEdit />} />
