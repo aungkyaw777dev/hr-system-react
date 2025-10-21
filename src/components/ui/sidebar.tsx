6c597f8ec0c1a2340c222101d64bba782cc8c80b
sawmon71293 <sawmon71293@gmail.com>
2025-10-21 09:58:56 +0630

    sidebar ui bug

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -8,13 +8,14 @@ import {
   DollarSign,
   LayoutTemplate,
   Clock,
-  ChevronUp
-} from 'lucide-react';
+  ChevronUp,
+} from "lucide-react";
 import { useState } from "react";
 
 export default function Sidebar({ onClose }: { onClose: () => void }) {
   const [isBacklogSubMenuOpen, setIsBacklogSubMenuOpen] = useState(false);
-  const toggleBacklogSubmenu = () => setIsBacklogSubMenuOpen(!isBacklogSubMenuOpen);
+  const toggleBacklogSubmenu = () =>
+    setIsBacklogSubMenuOpen(!isBacklogSubMenuOpen);
   const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
   const toggleSubmenu = () => setIsSubMenuOpen(!isSubMenuOpen);
   const [isSubMenuItemOpen, setIsSubMenuItemOpen] = useState(false);
@@ -24,93 +25,101 @@ export default function Sidebar({ onClose }: { onClose: () => void }) {
   return (
     <div className="flex flex-col items-center gap-2">
       <Link to="/dashboard" onClick={onClose} className="sidebar-btn">
-        <LayoutDashboard />Dashboard
+        <LayoutDashboard />
+        Dashboard
       </Link>
       <Link to="/role" onClick={onClose} className="sidebar-btn">
-        <UserRound />Role
+        <UserRound />
+        Role
       </Link>
       <div
         className="sidebar-btn flex w-full justify-between"
-        onClick={() =>
-          toglemenuItem()
-        }
+        onClick={() => toglemenuItem()}
       >
-        <span className="cursor-pointer flex gap-1"><LayoutDashboard />Menu</span>
+        <span className="cursor-pointer flex gap-1">
+          <LayoutDashboard />
+          Menu
+        </span>
         <ChevronUp
-          className={`mt-2 text-sm transition-transform duration-300 ${isSubMenuItemOpen ? "rotate-180" : "rotate-0"
-            }`}
+          className={`mt-2 text-sm transition-transform duration-300 ${
+            isSubMenuItemOpen ? "rotate-180" : "rotate-0"
+          }`}
           size={14}
         />
       </div>
-      {
-        isSubMenuItemOpen && (
-          <ul className="w-full">
-            <li className="sidebar-btn" onClick={onClose}>
-              <Link to="/menuitem" >Menu Item</Link>
-            </li>
-          </ul>
-        )
-      }
+      {isSubMenuItemOpen && (
+        <>
+          <Link to="/menuitem" className="sidebar-btn" onClick={onClose}>
+            Menu Item
+          </Link>
+        </>
+      )}
       <Link to="/role" onClick={onClose} className="sidebar-btn">
-        <UserRound />Role & Permission
+        <UserRound />
+        Role & Permission
       </Link>
       <Link to="/employee" onClick={onClose} className="sidebar-btn">
-        <UsersRound />Employee
+        <UsersRound />
+        Employee
       </Link>
       <div
         className="sidebar-btn w-full justify-between"
         onClick={toggleBackLogMenu}
       >
-        <span className="cursor-pointer flex gap-1"><LayoutTemplate />Backlog Module</span>
+        <span className="cursor-pointer flex gap-1">
+          <LayoutTemplate />
+          Backlog Module
+        </span>
         <ChevronUp
-          className={`mt-2 text-sm transition-transform duration-300 ${isBackLogMenuOpen ? "rotate-180" : "rotate-0"
-            }`}
+          className={`mt-2 text-sm transition-transform duration-300 ${
+            isBackLogMenuOpen ? "rotate-180" : "rotate-0"
+          }`}
           size={14}
         />
       </div>
-      {
-        isBackLogMenuOpen && (
-          <ul className="w-full">
-            <li className="sidebar-btn" onClick={onClose}>
-              <Link to="/backlog">Backlog</Link>
-            </li>
-            <li className="sidebar-btn" onClick={onClose}>
-              <Link to="/project">Project</Link>
-            </li>
-          </ul>
-        )
-      }
+      {isBackLogMenuOpen && (
+        <>
+          <Link to="/backlog" className="sidebar-btn" onClick={onClose}>
+            Backlog
+          </Link>
+          <Link to="/project" className="sidebar-btn" onClick={onClose}>
+            Project
+          </Link>
+        </>
+      )}
       <div
         className="sidebar-btn flex w-full justify-between"
         onClick={() => {
           toggleSubmenu();
         }}
       >
-        <span className="cursor-pointer flex gap-1"><Clock />Attendance Module</span>
+        <span className="cursor-pointer flex gap-1">
+          <Clock />
+          Attendance Module
+        </span>
         <ChevronUp
-          className={`mt-2 text-sm transition-transform duration-300 ${isSubMenuOpen ? "rotate-180" : "rotate-0"
-            }`}
+          className={`mt-2 text-sm transition-transform duration-300 ${
+            isSubMenuOpen ? "rotate-180" : "rotate-0"
+          }`}
           size={14}
         />
       </div>
-      {
-        isSubMenuOpen && (
-          <ul className="w-full">
-            <li className="sidebar-btn" onClick={onClose}>
-              <Link to="/location" >Location</Link>
-            </li>
-            <li className="sidebar-btn" onClick={onClose}>
-              <Link to="/attendance" >Attendance</Link>
-            </li>
-          </ul>
-        )
-      }
+      {isSubMenuOpen && (
+        <>
+          <Link to="/location" onClick={onClose} className="sidebar-btn">
+            Location
+          </Link>
+          <Link to="/attendance" onClick={onClose} className="sidebar-btn">
+            Attendance
+          </Link>
+        </>
+      )}
       <Link to="/payroll" onClick={onClose} className="sidebar-btn">
         <DollarSign /> Payroll
       </Link>
       <Link to="/logout" onClick={onClose} className="sidebar-btn">
         <LogOut /> Logout
       </Link>
-    </div >
+    </div>
   );
 }
