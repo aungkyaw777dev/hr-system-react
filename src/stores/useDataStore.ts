7be692a67c684dd6408a278350f1b54ef9ca7ade
sawmonhan <sawmonhan@vitheymfi.com>
2025-11-07 16:02:53 +0630

    minor

--- src/stores/useDataStore.ts
+++ src/stores/useDataStore.ts
@@ -39,12 +39,12 @@ export const useDataStore = create<DataStore>((set) => ({
       };
 
       const response = await fetch(url, options);
-
+      const data = await response.json();
       if (!response.ok) {
         const errorText = await response.text();
         throw new Error(`API Error ${response.status}: ${errorText}`);
       }
-      set({ data: response.json().data, loading: false });
+      set({ data: data, loading: false });
     } catch (err) {
       set({ error: err.message, loading: false });
     }
