c4ea0821dfe98d8115391fad03a90043b4c4ea89
sawmonhan <sawmonhan@vitheymfi.com>
2025-11-19 13:01:25 +0630

    minor

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,31 @@
-import { defineConfig,loadEnv  } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
-export default  ({mode}) => {
-  const env = loadEnv(mode, process.cwd(), '')
-return defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return defineConfig({
+    plugins: [
+      react({
+        babel: {
+          plugins: [["babel-plugin-react-compiler"]],
+        },
+      }),
+      tailwindcss(),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
-    }),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
     },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: env.VITE_API_URL,
-        changeOrigin: false,
-        secure: false
-      }
-    }
-  }
-});
-
-}
-
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  });
+};
